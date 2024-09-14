ee657f56884a8de582c723c9ba03abbce46b7c30
asadbekumarov <asadbekumarov922@gmail.com>
2024-09-15 02:59:59 +0500

    endd

---
+++ script.js
@@ -0,0 +1,56 @@
+// 1------------------------------------------
+// function removeChar(str) {
+//   return str.slice(1, -1);
+// }
+// console.log(removeChar("salom"));
+// 2-------------------------------------------
+// function repeatStr(n, s) {
+//   return s.repeat(n);
+// }
+// console.log(repeatStr(6, "hello"));
+// 3--------------------------------------------
+// function squareSum(numbers) {
+//   let sum = 0;
+//   for (let item of numbers) {
+//     sum += item * item;
+//   }
+//   return sum;
+// }
+// console.log(squareSum([1, 2, 2]));
+// 4-------------------------------------------------
+// const stringToNumber = function (str) {
+//   let num = +str;
+//   return num;
+// };
+// console.log(stringToNumber("1234"));
+// 5-------------------------------------------------------
+// function foo() {
+//   return "hello world!";
+// }
+// console.log(foo());
+// 6--------------------------------------------------------
+// function willHeroSurvive(bullets, dragons) {
+//   return bullets >= 2 * dragons;
+// }
+// console.log(willHeroSurvive(10, 6));
+// 7----------------------------------------------------------
+// function getMiddle(s) {
+//   const length = s.length;
+//   const medium = Math.floor(length / 2);
+//   if (length % 2 === 0) {
+//     return s.slice(medium - 1, medium + 1);
+//   } else {
+//     return s[medium];
+//   }
+// }
+// console.log(getMiddle("hello"));
+// 8-----------------------------------------------------
+// function printArray(array) {
+//   return array.join(",");
+// }
+// console.log(printArray(["h", "o", "l", "a"]));
+// 9--------------------------------------------------------------
+// function nameShuffler(str) {
+//     return str.split(" ").reverse().join(" ");
+//   }
+//   console.log(nameShuffler("john McClane"));
